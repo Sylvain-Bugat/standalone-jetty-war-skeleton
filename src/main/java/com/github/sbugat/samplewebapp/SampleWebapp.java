35c5c7485c3b14e6c6bc291007c388b311e2163b
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-11-16 23:58:10 +0100

    Add sample webapp servlet

---
+++ src/main/java/com/github/sbugat/samplewebapp/SampleWebapp.java
@@ -0,0 +1,28 @@
+package com.github.sbugat.samplewebapp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Sample class of a web-app servlet
+ *
+ * @author Sylvain Bugat
+ *
+ */
+public class SampleWebapp extends HttpServlet {
+
+	private static final long serialVersionUID = 1997452078400181160L;
+
+	@Override
+	protected void doGet(final HttpServletRequest httpServletRequest, final HttpServletResponse httpServletResponse) throws ServletException, IOException {
+
+		System.out.println("SampleWebapp doGet on:" + httpServletRequest.getRequestURI());
+		httpServletRequest.setAttribute("requestedURI", httpServletRequest.getRequestURI());
+		// Redirect all get request to index.jsp page
+		httpServletRequest.getRequestDispatcher("/index.jsp").forward(httpServletRequest, httpServletResponse);
+	}
+}
