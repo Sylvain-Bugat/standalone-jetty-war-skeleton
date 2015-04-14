eb508d87c97037d540c4e95fdc3ef1f58ebdb332
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-04-14 23:33:35 +0200

    Add annotation configuration in the Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,20 +3,24 @@ import java.security.ProtectionDomain;
 
 import org.eclipse.jetty.server.NetworkTrafficServerConnector;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.Configuration.ClassList;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-public class Main
-{
-	public static void main(String[] args) throws Exception {
-		Server server = new Server();
+public class Main {
 
-		NetworkTrafficServerConnector connector = new NetworkTrafficServerConnector( server );
+	public static void main(final String[] args) throws Exception {
+		final Server server = new Server();
+
+		final ClassList classList = ClassList.setServerDefault(server);
+		classList.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration", "org.eclipse.jetty.annotations.AnnotationConfiguration");
+
+		final NetworkTrafficServerConnector connector = new NetworkTrafficServerConnector(server);
 		connector.setPort(8080);
 		server.addConnector(connector);
 
-		ProtectionDomain domain = Main.class.getProtectionDomain();
-		URL location = domain.getCodeSource().getLocation();
-		WebAppContext webapp = new WebAppContext();
+		final ProtectionDomain domain = Main.class.getProtectionDomain();
+		final URL location = domain.getCodeSource().getLocation();
+		final WebAppContext webapp = new WebAppContext();
 		webapp.setContextPath("/");
 		webapp.setWar(location.toExternalForm());
 		server.setHandler(webapp);
