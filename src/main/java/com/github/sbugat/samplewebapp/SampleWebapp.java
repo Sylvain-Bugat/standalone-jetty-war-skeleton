b06c832acb885e07eb5d988459baae97ea0fe322
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-04-14 23:39:45 +0200

    Change default webapp

--- src/main/java/com/github/sbugat/samplewebapp/SampleWebapp.java
+++ src/main/java/com/github/sbugat/samplewebapp/SampleWebapp.java
@@ -3,15 +3,17 @@ package com.github.sbugat.samplewebapp;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  * Sample class of a web-app servlet
- *
+ * 
  * @author Sylvain Bugat
  */
+@WebServlet("/redirect")
 public class SampleWebapp extends HttpServlet {
 
 	private static final long serialVersionUID = 1997452078400181160L;
@@ -22,6 +24,6 @@ public class SampleWebapp extends HttpServlet {
 		System.out.println("SampleWebapp class: doGet method called on:" + httpServletRequest.getRequestURI());
 		httpServletRequest.setAttribute("requestedURI", httpServletRequest.getRequestURI());
 		// Redirect all get request to index.jsp page
-		httpServletRequest.getRequestDispatcher("/index.jsp").forward(httpServletRequest, httpServletResponse);
+		httpServletRequest.getRequestDispatcher("/index.html").forward(httpServletRequest, httpServletResponse);
 	}
 }
